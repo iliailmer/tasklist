6e978ecd694fbc21bb3f5acb9eb3b943a0947009
Ilia <i.ilmer@yahoo.com>
2025-03-09 17:43:00 -0500

    add project title inference by dir name

--- src/main.rs
+++ src/main.rs
@@ -6,42 +6,29 @@ use crate::cli::{Cli, Commands};
 use crate::manager::Mngr;
 use clap::Parser;
 
+use std::fs;
 use std::path::PathBuf;
 
 fn get_tasklist_path(custom: Option<String>) -> (String, String) {
-    if let Some(path) = custom {
-        let path_buf = PathBuf::from(path);
-        let path_string = path_buf.to_string_lossy().to_string();
-        let title = match path_buf.parent() {
-            Some(parent) => parent
-                .file_name()
-                .map(|os_str| os_str.to_string_lossy().to_string())
-                .unwrap_or_else(|| parent.to_string_lossy().to_string()),
-            None => ".".to_string(),
-        };
-        (path_string, title)
-    } else if PathBuf::from(".tasklist").exists() {
-        let path_buf = PathBuf::from(".tasklist");
-        let path_string = path_buf.to_string_lossy().to_string();
-        let title = match path_buf.parent() {
-            Some(parent) => parent
-                .file_name()
-                .map(|os_str| os_str.to_string_lossy().to_string())
-                .unwrap_or_else(|| parent.to_string_lossy().to_string()),
-            None => String::from("."),
-        };
-        (path_string, title)
-    } else {
-        let path_buf = home::home_dir().unwrap().join(".tasklist");
-        let title = String::from("My Tasks");
-        (path_buf.to_string_lossy().to_string(), title)
-    }
+    let raw_path = custom.unwrap_or_else(|| ".tasklist".to_string());
+
+    let path_buf = fs::canonicalize(raw_path).unwrap_or_else(|_| PathBuf::from(".tasklist"));
+    let path_string = path_buf.to_string_lossy().to_string();
+
+    let title = match path_buf.parent() {
+        Some(parent) => parent
+            .file_name()
+            .map(|os_str| os_str.to_string_lossy().to_string())
+            .unwrap_or_else(|| parent.to_string_lossy().to_string()),
+        None => ".".to_string(),
+    };
+
+    (path_string, title)
 }
 
 fn main() {
     let args = Cli::parse();
     let (tasklist_path, project_title) = get_tasklist_path(args.file);
-    println!("{}", project_title);
     let mngr = Mngr::new(tasklist_path, Some(project_title));
     match args.command {
         Commands::Add { description } => mngr.add_task(description.unwrap()),
